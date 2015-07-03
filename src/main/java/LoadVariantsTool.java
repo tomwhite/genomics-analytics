e9b37c0ec3b46f4463759e07853ab00688646fb4
Tom White <tom@cloudera.com>
2015-07-03 15:13:47 +0100

    Use specific

--- src/main/java/LoadVariantsTool.java
+++ src/main/java/LoadVariantsTool.java
@@ -1,20 +1,25 @@
-import org.apache.avro.Schema;
-import org.apache.avro.generic.GenericData;
+import java.io.IOException;
+import org.apache.crunch.MapFn;
 import org.apache.crunch.PCollection;
 import org.apache.crunch.PTable;
 import org.apache.crunch.Pipeline;
 import org.apache.crunch.PipelineResult;
 import org.apache.crunch.Source;
 import org.apache.crunch.impl.mr.MRPipeline;
+import org.apache.crunch.io.From;
+import org.apache.crunch.io.parquet.AvroParquetFileSource;
 import org.apache.crunch.lib.Sort;
 import org.apache.crunch.types.avro.Avros;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.util.Tool;
+import org.ga4gh.models.FlatVariant;
+import org.ga4gh.models.Variant;
 import org.kitesdk.data.CompressionType;
 import org.kitesdk.data.DatasetDescriptor;
 import org.kitesdk.data.Datasets;
+import org.kitesdk.data.Format;
 import org.kitesdk.data.Formats;
 import org.kitesdk.data.View;
 import org.kitesdk.data.crunch.CrunchDatasets;
@@ -40,40 +45,57 @@ public class LoadVariantsTool extends Configured implements Tool {
     Configuration conf = getConf();
 
     Path path = new Path(inputPath);
-    Schema schema = SchemaUtils.readSchema(path, conf);
-    Schema flatSchema = new Flattener().flatten(schema);
-    Source source = SchemaUtils.readSource(path, conf, schema);
+    Source<Variant> source = readSource(path, conf);
 
     Pipeline pipeline = new MRPipeline(CrunchPartitionTool.class, conf);
-    PCollection<GenericData.Record> records = pipeline.read(source);
+    PCollection<Variant> records = pipeline.read(source);
 
-    PCollection<GenericData.Record> flatRecords = records.parallelDo(
-        new CrunchFlattenTool.FlattenFn(flatSchema), Avros.generics(flatSchema));
+    PCollection<FlatVariant> flatRecords = records.parallelDo(
+        new FlattenVariantFn(), Avros.specifics(FlatVariant.class));
 
     DatasetDescriptor desc = new DatasetDescriptor.Builder()
-        .schema(schema)
+        .schema(FlatVariant.getClassSchema())
         .partitionStrategy(CrunchPartitionTool.readPartitionStrategy(partitionStrategyName))
         .format(Formats.PARQUET)
         .compressionType(CompressionType.Uncompressed)
         .build();
 
-    View<GenericData.Record> dataset = Datasets.create(outputPath, desc,
-        GenericData.Record.class).getDataset().with("sample_group", sampleGroup);
+    View<FlatVariant> dataset = Datasets.create(outputPath, desc,
+        FlatVariant.class).getDataset().with("sample_group", sampleGroup);
 
     int numReducers = conf.getInt("mapreduce.job.reduces", 1);
     System.out.println("Num reducers: " + numReducers);
-    PCollection<GenericData.Record> partition =
+    PCollection<FlatVariant> partition =
         CrunchDatasets.partition(flatRecords, dataset, numReducers);
 
-    PTable<String, GenericData.Record> keyed = partition.by(
-        new CrunchPartitionTool.ExtractSortKeyFn(), Avros.strings());
-    PCollection<GenericData.Record> sorted = Sort.sort(keyed).values();
+    PTable<String, FlatVariant> keyed = partition.by(
+        new ExtractSortKeyFn(), Avros.strings());
+    PCollection<FlatVariant> sorted = Sort.sort(keyed).values();
 
     pipeline.write(sorted, CrunchDatasets.asTarget(dataset));
 
     PipelineResult result = pipeline.done();
     return result.succeeded() ? 0 : 1;
 
+  }
+
+  private static Source<Variant> readSource(Path path, Configuration conf) throws
+      IOException {
+    Path file = SchemaUtils.findFile(path, conf);
+    Format format = SchemaUtils.readFormat(file);
+    if (format == Formats.AVRO) {
+      return From.avroFile(path, Avros.specifics(Variant.class));
+    } else if (format == Formats.PARQUET) {
+      return new AvroParquetFileSource(path, Avros.specifics(Variant.class));
+    }
+    throw new IllegalStateException("Unrecognized format for " + file);
+  }
+
+  private static class ExtractSortKeyFn extends MapFn<FlatVariant, String> {
+    @Override
+    public String map(FlatVariant flatVariant) {
+      return flatVariant.getCallSetName().toString();
+    }
   }
 
 }
